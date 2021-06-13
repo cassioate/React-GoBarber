f83a2ff46d8eaaeed5fd3a48bca6280299c2434d
Cassio <cassioate@gmail.com>
2021-06-13 20:54:16 -0300

    Inserindo estilo no layout | utilizando Layout no SignIn e SignUp | inserindo biblioteca @rocketseat/unform para fazer os formularios | inserindo validações nos formularios utilizanod o Yup

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,9 +1,25 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { Input, Form } from '@rocketseat/unform'
 
-// import { Container } from './styles';
+import logo from '../../assets/logo.svg'
 
 function SigIn() {
-   return <h1>Login</h1>
+   function handleSubmit(data) {
+      console.tron.log(data)
+   }
+
+   return (
+      <>
+         <img src={logo} alt="GoBarber"></img>
+         <Form onSubmit={handleSubmit}>
+            <Input name="email" type="email" placeholder="Seu e-mail"></Input>
+            <Input name="password" type="password" placeholder="Digite sua senha"></Input>
+            <button type="submit">Acessar</button>
+            <Link to="/register">Criar sua conta</Link>
+         </Form>
+      </>
+   )
 }
 
 export default SigIn
--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,9 +1,32 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { Input, Form } from '@rocketseat/unform'
+import * as Yup from 'yup'
 
-// import { Container } from './styles';
+import logo from '../../assets/logo.svg'
+
+const schema = Yup.object().shape({
+   email: Yup.string().email('Insira um email valido.').required('Email é obrigatorio.'),
+   password: Yup.string().required('Senha é obrigatoria.'),
+})
 
 function SignUp() {
-   return <h1>Registro</h1>
+   function handleSubmit(data) {
+      console.tron.log(data)
+   }
+
+   return (
+      <>
+         <img src={logo} alt="GoBarber"></img>
+         <Form schema={schema} onSubmit={handleSubmit}>
+            <Input name="nome" type="text" placeholder="Nome Completo"></Input>
+            <Input name="email" type="email" placeholder="Seu e-mail"></Input>
+            <Input name="password" type="password" placeholder="Digite sua senha"></Input>
+            <button type="submit">Criar conta</button>
+            <Link to="/">Já possuo login</Link>
+         </Form>
+      </>
+   )
 }
 
 export default SignUp
